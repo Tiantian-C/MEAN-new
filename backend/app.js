bc0a40cdfdcaf95575097200e31923d600ac4f57
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-08-11 21:32:53 -0700

    56

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://yanshubaobei2023:SHo2CPsKrFsaLfZA@cluster0.nzdk0az.mongodb.net/"
+).then(() => {
+  console.log('Connected to database!');
+}).catch(() => {
+  console.log('Connnection failed!');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,30 +31,24 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
-    console.log(post);
-    res.status(201).json({
-        message:'Post added successfully!'
-    })
-})
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
+  res.status(201).json({
+    message: "Post added successfully!",
+  });
+});
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fsghsjjkk12",
-      title: "first server-side post",
-      content: "This is coming from server",
-    },
-    {
-      id: "fsgkvhktttt12",
-      title: "second server-side post",
-      content: "This is coming from server!",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts,
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents,
+    });
   });
+  
 });
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -12,11 +13,20 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
+      .get<{ message: string; posts: any }>(
         'http://localhost:3000/api/posts'
-      )
-      .subscribe((postData) => {
-          this.posts = postData.posts;
+    )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id:post._id
+          }
+        });
+      }))
+      .subscribe((transformedPosts) => {
+          this.posts = transformedPosts;
           this.postsUpdated.next([...this.posts]);
       });
   }

31d4e320540c5666c7ecadc51edd9e6bda287c1d
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-08-15 00:00:03 -0700

    111

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,14 +3,16 @@ import { HttpClient } from '@angular/common/http';
 
 import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private isAuthenticated = false;
   private token: string;
+  private tokenTimer: any;
   private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private router:Router) { }
   
   getToken() {
     return this.token;
@@ -30,26 +32,37 @@ export class AuthService {
       .post('http://localhost:3000/api/user/signup', authData)
       .subscribe((response) => {
         console.log(response);
+        this.router.navigate(['/']);
       });
+    
   }
 
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post<{token:string}>('http://localhost:3000/api/user/login', authData)
-      .subscribe(response => {
+    this.http
+      .post<{ token: string; expiresIn:number}>(
+        'http://localhost:3000/api/user/login',
+        authData
+      )
+      .subscribe((response) => {
         const token = response.token;
         this.token = token;
         if (token) {
-          this.isAuthenticated= true;
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => {this.logout()}, expiresInDuration * 1000);
+          this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          this.router.navigate(['/']);
         }
-    })
+      });
   }
 
   logout() {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
+    this.router.navigate(['/']); 
   }
 
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -14,6 +14,8 @@ export class SignupComponent {
     if (form.invalid) {
       return;
     }
+    this.isLoading = true;
+    
     this.authService.createUser(form.value.email, form.value.password);
   }
 }
